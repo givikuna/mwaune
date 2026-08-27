bb5a7a2e0827dffb612cbe891275268ab6d1474a
Givi <gvtsvariani@gmail.com>
2026-08-26 20:49:44 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mwaune
+
 Book reading and management software for local book repositories. Reads pdfs, epubs, and such.
--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
   };
 
   outputs =
@@ -11,19 +12,76 @@
       self,
       nixpkgs,
       flake-utils,
+      naersk,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        naersk' = pkgs.callPackage naersk { };
+
+        node = pkgs.nodejs_latest;
+        npm = pkgs.nodePackages.npm;
+
+        tauriApp = naersk'.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gtk4
+            webkitgtk_6_0
+            libsoup_3
+            glib
+            gdk-pixbuf
+            librsvg
+
+            nodejs_latest
+            nodePackages_latest.npm
+          ];
+          buildInputs = with pkgs; [
+            openssl
+            dbus
+            atk
+            gtk3
+            glib
+            cairo
+            pango
+            gdk-pixbuf
+            webkitgtk_6_0
+            librsvg
+          ];
+
+          preBuild = ''
+            npm install
+            npm run build
+          '';
+
+          TAURI_SKIP_BUILD = "1";
+          TAURI_WEBVIEW = "webkit2gtk"; # linux
+        };
       in
       {
+        packages.default = tauriApp;
+        apps.default = {
+          type = "app";
+          program = "${tauriApp}/bin/mwaune";
+        };
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            clippy
             nodejs_latest
-            yarn
-            electron
-            typescript
+            pkg-config
+            gtk4
+            webkitgtk_6_0
+            libsoup_3
+            glib
+            gdk-pixbuf
+            librsvg
+            openssl
+            dbus
           ];
 
           shellHook = ''
